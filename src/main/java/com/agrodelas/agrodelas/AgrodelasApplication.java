f161972acdd35a846bdeaab9c756dfd4cbadf688
geovanidomingos <geovani.domingos@mercadolivre.com>
2023-02-02 10:13:13 -0300

    Primeiro Commit

---
+++ src/main/java/com/agrodelas/agrodelas/AgrodelasApplication.java
@@ -0,0 +1,13 @@
+package com.agrodelas.agrodelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrodelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrodelasApplication.class, args);
+	}
+
+}
