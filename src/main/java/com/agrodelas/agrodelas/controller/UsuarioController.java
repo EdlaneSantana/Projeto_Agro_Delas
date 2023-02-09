4dabddc74555e424c776173335d6fdf10d1570e2
˜Edlane <edlane.santana@mercadolivre.com>
2023-02-09 16:55:11 -0300

    Alterando a rota do UsuarioController para 'usuarios'

--- src/main/java/com/agrodelas/agrodelas/controller/UsuarioController.java
+++ src/main/java/com/agrodelas/agrodelas/controller/UsuarioController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/usuarios")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
 
