a25d8e3fc815f95dc1a1d91766ca5116e316bf43
anna-szulzik <annaszulzik10@gmail.com>
2023-02-16 10:57:49 -0300

    deploy

--- src/main/java/com/agrodelas/agrodelas/model/Usuario.java
+++ src/main/java/com/agrodelas/agrodelas/model/Usuario.java
@@ -2,6 +2,7 @@ package com.agrodelas.agrodelas.model;
 
 import com.agrodelas.agrodelas.model.Produto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -30,6 +31,7 @@ public class Usuario {
 
     @NotBlank(message = "O atributo e-mail é obrigatório")
     @Email(message = "O usuário deve inserir um e-mail válido!")
+    @Schema(example = "email@email.com.br")
     private String email;
 
     @NotBlank(message = "O atributo Rua é obrigatório")
